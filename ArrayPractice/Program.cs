8385908ff6c9c07c1fe8fef22085812e58c3118b
awallbark <awallbark@gmail.com>
2021-11-22 16:00:04 -0600

    Adds do while loop that does the same thing as the commented out for loop

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -6,12 +6,16 @@ namespace ArrayPractice
     {
         static void Main(string[] args)
         {
+            //Practice problem from https://www.csharp-console-examples.com/arrays/c-array-exercises/
+
             //declaring a variable to carry the size of the array
             int arraySize;
 
             //gathering the information to set the Array Size
-            Console.WriteLine("Enter the number for the size for your array.");
+            Console.WriteLine("Enter the number for the size for your array:");
             arraySize = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The size of the array is {0}", arraySize);
 
             //initalizing the Array and setting it's size
             int[] someNumbers = new int[arraySize];
@@ -19,12 +23,21 @@ namespace ArrayPractice
             //Adding a space to the printed results
             Console.WriteLine();
 
-            //looping through to add elements to the array
-            for (int i = 0; i < someNumbers.Length; i++)
+            Console.WriteLine("Enter {0} elements into the array", arraySize);
+
+            int i = 0;
+            do
             {
-                Console.WriteLine("Please enter an integer");
                 someNumbers[i] = Convert.ToInt32(Console.ReadLine());
-            }
+                i++;
+            } while (i < someNumbers.Length);
+
+            //looping through to add elements to the array
+            //for (int i = 0; i < someNumbers.Length; i++)
+            //{
+            //    Console.WriteLine("Please enter an integer");
+            //    someNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
 
             //Adding a space to the printed results
             Console.WriteLine();
