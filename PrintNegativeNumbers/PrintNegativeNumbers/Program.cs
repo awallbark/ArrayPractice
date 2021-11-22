ea06873a64ad718af5a73b0033846e60a2c8d8ab
awallbark <awallbark@gmail.com>
2021-11-22 16:23:52 -0600

    Creates a program to print negative numbes in an array.

---
+++ PrintNegativeNumbers/PrintNegativeNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PrintNegativeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize;
+
+            Console.WriteLine("Enter the size of your array");
+            arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] negativeArray = new int[arraySize];
+
+            Console.WriteLine("Please enter positive and negative integers");
+
+            for (int i = 0; i < negativeArray.Length; i++)
+            {
+                negativeArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The negative elements of the array are as follows.....");
+
+            for (int i = 0; i < negativeArray.Length; i++)
+            {
+                int j = negativeArray[i];
+
+                if (j < 0)
+                {
+                    Console.WriteLine(negativeArray[i]);
+                }
+            }
+      
+        }
+    }
+}
